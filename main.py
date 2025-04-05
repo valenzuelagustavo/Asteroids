84bd5ff694c91390136365ae736ae638ffde0515
valenzuelagustavo <123873650+valenzuelagustavo@users.noreply.github.com>
2025-04-05 12:14:02 +0000

    Add the logic to managing the FPS (60)

--- main.py
+++ main.py
@@ -11,14 +11,18 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}") 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    #Manejo de FPS
+    clock = pygame.time.Clock()
+    dt = 0
+
     while 1 > 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
         screen.fill("black")
-        pygame.display.flip()
-        
+        pygame.display.flip()        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
