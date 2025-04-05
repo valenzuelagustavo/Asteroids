78c2f579bf924bdda6c8456b536b1984bb3ca70c
valenzuelagustavo <123873650+valenzuelagustavo@users.noreply.github.com>
2025-04-05 14:18:54 +0000

    Adding player sprite to display

---
+++ circleshape.py
@@ -0,0 +1,23 @@
+import pygame
+
+PLAYER_RADIUS = 20
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def update(self, dt):
+        # sub-classes must override
+        pass
--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.display.set_mode()
@@ -10,19 +12,23 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}") 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    #Manejo de FPS
+    #manage the fps
     clock = pygame.time.Clock()
     dt = 0
 
+    #manage the game loop
     while 1 > 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()        
         dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
