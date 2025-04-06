8046c7bb6d9f9629f482d76adb1fc358f7b541b4
valenzuelagustavo <123873650+valenzuelagustavo@users.noreply.github.com>
2025-04-06 01:33:15 +0000

    Adding rotation to the spaceship

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ def main():
                 return
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()        
         dt = clock.tick(60) / 1000
         
